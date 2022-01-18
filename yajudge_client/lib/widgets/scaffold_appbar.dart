6eb0ed148633d8cb728676913f55cf88e5eae9db
Victor Yacovlev <v.yacovlev@gmail.com>
2022-01-18 19:06:52 +0300

    cleanup before rework

--- yajudge_client/lib/widgets/scaffold_appbar.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:yajudge_client/app.dart';
-import 'package:yajudge_client/wsapi/connection.dart';
-import 'package:yajudge_client/wsapi/users.dart';
-
