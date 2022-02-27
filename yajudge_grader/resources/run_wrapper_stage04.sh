e053c7d9195f124219e281a0a18facfd7c77b688
Victor Yacovlev <v.yacovlev@gmail.com>
2022-02-28 01:51:39 +0300

    grader: local network in containers works while global networking disabled

--- yajudge_grader/resources/run_wrapper_stage04.sh
+++ yajudge_grader/resources/run_wrapper_stage04.sh
@@ -4,18 +4,12 @@
 YAJUDGE_SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 export YAJUDGE_SCRIPT_STAGE=04
 
-# Stage 4: while unshared everything but chroot, run supplementing services and than go next stage
+# Stage 4: if network unshared, setup new localhost interface
 
-# move this process to control group
-#echo "$$" > "$YAJUDGE_CGROUP_PATH/cgroup.procs"
-
-for service in $YAJUDGE_SERVICES
-do
-  unshare -U --kill-child=SIGTERM --mount-proc \
-    --root="$YAJUDGE_ROOT_DIR" \
-    --wd="$YAJUDGE_WORK_DIR" \
-    "$service"
-done
+if [ -z "$YAJUDGE_ALLOW_NETWORK" ]
+then
+  ip link set dev lo up
+fi
 
 if [ -n "$YAJUDGE_DEBUG" ]; then echo $YAJUDGE_SCRIPT_STAGE; fi
 exec unshare -pf bash "$YAJUDGE_SCRIPT_DIR/run_wrapper_stage05.sh" $@
