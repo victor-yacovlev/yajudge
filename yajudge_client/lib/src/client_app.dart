9d606fa5fb2281d1c52ad029662c23275f38394f
Victor Yacovlev <v.yacovlev@gmail.com>
2022-07-07 17:42:02 +0300

    fixed js target build

--- yajudge_client/lib/src/client_app.dart
+++ yajudge_client/lib/src/client_app.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: unused_local_variable
 
-import 'dart:ffi';
-
 import 'package:fixnum/fixnum.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
